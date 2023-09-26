42947485e59bb3cb035d531a13ea2aed82047f00
hhhikako <133066299+hhhikako@users.noreply.github.com>
2023-09-26 14:18:39 +0900

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,24 +1,11 @@
 
-// Personalized welcome message code
+const myImage = document.querySelector("img");
 
-let myButton = document.querySelector('button');
-let myHeading = document.querySelector('h1');
-
-function setUserName() {
-  const myName = prompt("あなたの名前を入力してください。");
-  localStorage.setItem("name", myName);
-  myHeading.textContent = `Mozilla はかっこいいよ、${myName} さん`;
-}
-
-myButton.onclick = function() {
-  setUserName();
-}
-
-// Image switcher code
-
-let myImage = document.querySelector('img');
-
-myImage.onclick = function() {
-  let mySrc = myImage.getAttribute('src');
-  console.log("画像をクリックしました");
-}
+myImage.onclick = () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/firefox-icon.png") {
+    myImage.setAttribute("src", "images/firefox2.png");
+  } else {
+    myImage.setAttribute("src", "images/firefox-icon.png");
+  }
+};
